5e9f583230defd6f9cd2ddc029fc306f782f8d56
Kyle Robertson <kyle.wesley@me.com>
2018-05-13 20:26:52 -0400

    Minor mods. Added node_config boot scripts

--- node_config.sh
+++ node_config.sh
@@ -4,4 +4,3 @@ apt update -y
 apt upgrade -y
 apt install -y python-pip python3 python3-pip
 pip install ansible python-openstackclient
-#openstack server list -f csv | cut -d"," -f4 | cut -d"=" -f 2 | tr -d '"'
